df1be4bb3ea09b4efe2996fe8ac046e67da77c79
Malar Invention <malarkannan.invention@gmail.com>
2024-07-22 18:55:32 +0530

    update namespace and ubuntu version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 source ./VERSION
 VERSION="${BASE_VERSION}-${SUFFIX_VERSION}"
 
-(cd argo-cd && IMAGE_NAMESPACE=ghcr.io/flux-subsystem-argo/fsa IMAGE_TAG=$VERSION DOCKER_PUSH=true make image)
+(cd argo-cd && IMAGE_NAMESPACE=ghcr.io/malarinv/fsa IMAGE_TAG=$VERSION DOCKER_PUSH=true make image)
